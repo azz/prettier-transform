c64d332e4fedc73f43184d538644a0c1320e6dff
Lucas Azzola <derflatulator@gmail.com>
2017-09-29 20:23:01 +1000

    feat(cli): implement CLI

--- README.md
+++ README.md
@@ -5,4 +5,42 @@
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg?style=flat-square)](https://github.com/semantic-release/semantic-release)
 [![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](LICENSE)
 
-TODO(azz): docs :)
+## Install
+
+```bash
+yarn add --dev prettier-transform
+```
+
+## CLI Usage
+
+Basic usage:
+
+```bash
+prettier-transform file.js --parser babylon -t ./my-transform
+```
+
+Unknown options will be passed to prettier:
+
+```bash
+prettier-transform file.js --parser babylon -t ./my-transform --no-semi
+```
+
+## API Usage
+
+Basic usage:
+
+```js
+const prettierTransform = require("prettier-transform");
+const myTransform = require("./my-transform");
+
+prettierTransform("foo()", [myTransform], { parser: "babylon" });
+```
+
+Pass options to prettier:
+
+```js
+prettierTransform("foo()", [myTransform], {
+  parser: "babylon",
+  semi: false
+});
+```
--- bin/prettier-transform.js
+++ bin/prettier-transform.js
@@ -2,5 +2,75 @@
 
 "use strict";
 
-// TODO(azz):
+const camelcaseKeys = require("camelcase-keys");
+const minimist = require("minimist");
+const resolve = require("resolve");
+const assert = require("assert");
+const globby = require("globby");
+const path = require("path");
+const fs = require("fs");
 
+const prettierTransform = require("../");
+
+const args = normalizeArgs(parseArgv(process.argv.slice(2)));
+
+assert.ok(typeof args.parser === "string", "--parser must be a string");
+
+const transforms = args.transform.map(loadTransform);
+const filePaths = getFilePaths(args.patterns);
+
+delete args.prettierOptions.t;
+delete args.prettierOptions._;
+
+for (const code of filePaths.map(readFile)) {
+  const output = prettierTransform.format(
+    code,
+    transforms,
+    Object.assign({}, args.prettierOptions, {
+      parser: args.parser
+    })
+  );
+  console.log(output);
+}
+
+function parseArgv(argv) {
+  return minimist(argv, {
+    string: ["t", "parser"],
+    default: {
+      t: []
+    }
+  });
+}
+
+function normalizeArgs(args) {
+  return Object.assign({}, args, {
+    transform: [].concat(args.t || []),
+    patterns: [].concat(args._ || []),
+    prettierOptions: camelcaseKeys(args)
+  });
+}
+
+function getFilePaths(patterns) {
+  return globby
+    .sync(patterns, { dot: true })
+    .map(
+      filePath =>
+        path.isAbsolute(filePath)
+          ? path.relative(process.cwd(), filePath)
+          : filePath
+    );
+}
+
+function loadTransform(modulePath) {
+  const fsPath = resolve.sync(modulePath, { basedir: process.cwd() });
+  return require(fsPath);
+}
+
+function readFile(filePath) {
+  return fs.readFileSync(
+    path.isAbsolute(filePath)
+      ? filePath
+      : path.relative(process.cwd(), filePath),
+    "utf8"
+  );
+}
